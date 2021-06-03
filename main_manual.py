a11f8f7e59005433111e11996fbcbb4518c0037c
Louis <louis8ok1@gmail.com>
2021-06-04 01:54:53 +0800

    Final version

--- main_manual.py
+++ main_manual.py
@@ -9,10 +9,11 @@ test_data = np.zeros((10000,50))
 
 new_train_data = np.zeros((10000,50))
 new_test_data = np.zeros((10000,50))
+
 def open_data():
     train_data = np.load('./data/train_data.npy')
     test_data  = np.load('./data/test_data.npy')
-    #print(train_data)
+    print(train_data.shape)
     return train_data,test_data
 
 def label_data():
@@ -28,7 +29,8 @@ def concatenate_Data(x_train,x_test,y_train,y_test):
     new_train_data = np.concatenate((x_train,y_train),axis = 1)
     new_test_data  = np.concatenate((x_test,y_test),axis = 1)
    # print(new_train_data)
-    print(new_train_data.shape)
+    
+   
     return new_train_data,new_test_data
 
 def mean(cls1_test_data,cls2_test_data):
@@ -38,19 +40,24 @@ def mean(cls1_test_data,cls2_test_data):
     temp1 = temp1[:50]
     temp2 = np.mean(cls2_test_data, axis=0)
     temp2 = temp2[:50]
+    print('\ntemp1')
+    print(temp1)
+    print('\ntemp2')
+    print(temp2)
     return temp1 , temp2
 
 def cov(cls1_test_data,cls2_test_data):
-    
+    print('\n Conv1')
+    print(np.cov(cls1_test_data[:,:50].T))
     return np.cov(cls1_test_data[:,:50].T),np.cov(cls2_test_data[:,:50].T)
 
-def test_data_split(new_test_data):
+def data_split(data):
     #split data
     cls1_data = np.array([[]])
     cls2_data = np.array([[]])
     
-    cls1_data = new_test_data[:5000,:]
-    cls2_data =new_test_data[5000:10000,:]
+    cls1_data = data[:5000,:]
+    cls2_data =data[5000:10000,:]
     return cls1_data,cls2_data
 
 def PDF(x,mu,cov):
@@ -61,15 +68,16 @@ def PDF(x,mu,cov):
              diff_x_mu = np.append(diff_x_mu,[[ float(x[i][0])-mu[i] ]],axis=1)
          else :
              diff_x_mu = np.append(diff_x_mu,[[ float(x[i][0])-mu[i] ]],axis=0)
-     #print(diff_x_mu.shape)       
+     #print(diff_x_mu.shape)  
+  
      covinv = np.linalg.inv(cov)
      temp = np.dot(diff_x_mu.T,covinv)
-     temp = np.dot(temp,diff_x_mu)
-     tempexp = math.exp(temp*(-1/2))
-     tempdet = np.linalg.det(cov)
-     tempdet = math.pow(tempdet,0.5)
-     demo = math.pow((math.pi)*2,25)
-     demo *= tempdet
+     temp2 = np.dot(temp,diff_x_mu)
+     tempexp = math.exp(temp2*(-1/2))
+     demo = math.pow((math.pi)*2,50)
+     covdet = np.linalg.det(cov)
+     demo*= covdet 
+     demo = math.pow(demo,(1/2))
      p = tempexp/demo
      return p
 def test_data_predict(cls1_test_data,cls2_test_data,mean1,mean2,cov1,cov2):
@@ -81,6 +89,7 @@ def test_data_predict(cls1_test_data,cls2_test_data,mean1,mean2,cov1,cov2):
     print(cls1_test_data.shape)
     #test data, label is class 1 
     correct1 = 0 
+    correct2 = 0 
     for j in range(5000):
         x = np.array([[]])
         for i in range(50):
@@ -95,9 +104,9 @@ def test_data_predict(cls1_test_data,cls2_test_data,mean1,mean2,cov1,cov2):
         P_x_2 = PDF(x,mean2,cov2)
         if P_x_1 >=P_x_2 :
             correct1 +=1
-    
+            
     #test data, label is class 2
-    correct2 = 0 
+    
     for j in range(5000):
         x = np.array([[]])
         for i in range(50):
@@ -116,6 +125,17 @@ def test_data_predict(cls1_test_data,cls2_test_data,mean1,mean2,cov1,cov2):
     print("class1 test correct : ",correct1)
     print("class2 test correct : ",correct2)
     print("Accuracy of prediction : ", round(((correct1+correct2)/10000)*100,2),"%" )
+
+col,row = 50,50
+
+
+def cal_CovrianceMatrix(var):
+    arrayA = np.zeros((col,row))
+    for i in range(col):
+        for j in range(row):
+            arrayA[i][j]=(float)(var**(abs(i-j)))
+    
+    return arrayA
     
 if __name__ =="__main__":
     
@@ -126,18 +146,19 @@ if __name__ =="__main__":
       
     #concatenate data & label
     new_train_data,new_test_data = concatenate_Data(x_train,x_test,y_train,y_test)
-    print(new_test_data.shape)
+    #data split
+    cls1_test_data,cls2_test_data = data_split(new_test_data)
     
-    
-     
-    cls1_test_data,cls2_test_data = test_data_split(new_test_data)
+    cls1_train_data,cls2_train_data = data_split(new_train_data)
     
     #mean vector 50*1
-    mean1,mean2 = mean(cls1_test_data,cls2_test_data)
+    mean1,mean2 = mean(cls1_train_data,cls2_train_data)
     
     #covariance matrix 50*50
-    cov1,cov2 = cov(cls1_test_data,cls2_test_data)
+    cov1,cov2 = cov(cls1_train_data,cls2_train_data)
     
+
     #test data predict
     test_data_predict(cls1_test_data,cls2_test_data,mean1,mean2,cov1,cov2)
-    
+    #train data predict
+    test_data_predict(cls1_train_data,cls2_train_data,mean1,mean2,cov1,cov2)
